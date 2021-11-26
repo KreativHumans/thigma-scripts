06235f1afebbe07e6fd42012ec72cde2613aed05
Chamaru Amasara <mailtochamaru@gmail.com>
2021-11-26 11:31:43 +0530

    Update randomNumGenerator.py

--- randomNumGenerator.py
+++ randomNumGenerator.py
@@ -14,7 +14,7 @@ try:
     import os
 
     # Get the current working directory
-    cwd = os.getcwd()+"/"
+    cwd = os.getcwd()
 
     #--------------------character lists-------------------
     chars=string.digits+string.ascii_uppercase
@@ -28,9 +28,13 @@ try:
         "Enrolment Key Generator"
         return "-".join(''.join(random.SystemRandom().choice(chars) for _ in range(delimiter)) for k in range(length//delimiter))
 
-    def savetoFile(filename,method,stringtoWrite):
+    def savetoFile(filename,filedir,method,stringtoWrite):
         "Saves data to a file and closes the file"
-        newEnrolmentKeys=open(cwd+filename,method)
+        place=os.path.join(cwd,filedir)
+        global filelocation
+        filelocation=os.path.join(place,filename)
+        filelocation.replace("\\\\","\\")
+        newEnrolmentKeys=open(filelocation,method)
         newEnrolmentKeys.write(stringtoWrite)
         newEnrolmentKeys.close()
         return
@@ -86,20 +90,20 @@ try:
     #Saving keys to respective files
 
     #File which should be imported to moodle
-    savetoFile("newKeys/newEnrolmentKeystoMoodle.csv","w","groupname,enrolmentkey\n"+generatedCompleteString)
+    savetoFile("newEnrolmentKeystoMoodle.csv","newKeys","w","groupname,enrolmentkey\n"+generatedCompleteString)
 
 
     #File which should be given to Thigma
-    savetoFile("newKeys/newEnrolmentKeystoThigma.csv","w",generatedKeysString)
+    savetoFile("newEnrolmentKeystoThigma.csv","newKeys","w",generatedKeysString)
 
 
 
     #Backup File of keys imported to moodle
-    savetoFile("backupKeys/backupnewEnrolmentKeystoMoodle.csv","a",generatedCompleteString)
+    savetoFile("backupnewEnrolmentKeystoMoodle.csv","backupKeys","a",generatedCompleteString)
 
 
     #Backup File of keys given to Thigma
-    savetoFile("backupKeys/backupnewEnrolmentKeystoThigma.csv","w",generatedKeysString)
+    savetoFile("backupnewEnrolmentKeystoThigma.csv","backupKeys","w",generatedKeysString)
 
 
 
@@ -108,5 +112,10 @@ try:
     print(f"\nSaved {howMany} keys to following files\n"+cwd+"newKeys/newEnrolmentKeystoMoodle.csv\n"+cwd+"newKeys/newEnrolmentKeystoThigma.csv\n"+cwd+"backupKeys/backupnewEnrolmentKeystoMoodle.csv\n"+cwd+"backupKeys/backupnewEnrolmentKeystoThigma.csv")
     x=input("Press any key to exit the program...")
 except Exception as e:
+    print("There was an error while running the program.")
     print(e)
+    try:
+        print(filelocation)
+    except:
+        pass
     x=input("Press any key to exit the program...")
